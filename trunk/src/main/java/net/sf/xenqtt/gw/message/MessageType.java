f524819c21ee0904315a0900c3746d2c632cd89a
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-08-15 02:40:00 +0000

    Interim commit of mqtt message classes

---
+++ trunk/src/main/java/net/sf/xenqtt/gw/message/MessageType.java
@@ -0,0 +1,23 @@
+package net.sf.xenqtt.gw.message;
+
+/**
+ * MQTT Message types. The order must not be changed as the enum ordinal() is the numeric value of the message type.
+ */
+public enum MessageType {
+	UNUSED_0, // Reserved
+	CONNECT, // Client request to connect to Server
+	CONNACK, // Connect Acknowledgment
+	PUBLISH, // Publish message
+	PUBACK, // Publish Acknowledgment
+	PUBREC, // Publish Received (assured delivery part 1)
+	PUBREL, // Publish Release (assured delivery part 2)
+	PUBCOMP, // Publish Complete (assured delivery part 3)
+	SUBSCRIBE, // Client Subscribe request
+	SUBACK, // Subscribe Acknowledgment
+	UNSUBSCRIBE, // Client Unsubscribe request
+	UNSUBACK, // Unsubscribe Acknowledgment
+	PINGREQ, // PING Request
+	PINGRESP, // PING Response
+	DISCONNECT, // Client is Disconnecting
+	UNUSED_15 // Reserved
+}
