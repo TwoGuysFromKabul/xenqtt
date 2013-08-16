8add7182771ca5b9a9e80d1a023be81074f198cc
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-08-16 02:16:17 +0000

    Refactored MqttChannel. Added PubRecMessage. Finished MessageHandler interface.

--- trunk/src/main/java/net/sf/xenqtt/message/MessageHandler.java
+++ trunk/src/main/java/net/sf/xenqtt/message/MessageHandler.java
@@ -14,5 +14,64 @@ public interface MessageHandler {
 	 * Called when a {@link ConnAckMessage} is received
 	 */
 	void handle(ConnAckMessage message);
-	// FIXME [jim] - need to add methods for other message types
+
+	/**
+	 * Called when a {@link PublishMessage} is received
+	 */
+	void handle(PublishMessage message);
+
+	/**
+	 * Called when a {@link PubAckMessage} is received
+	 */
+	void handle(PubAckMessage message);
+
+	/**
+	 * Called when a {@link PubRecMessage} is received
+	 */
+	void handle(PubRecMessage message);
+
+	/**
+	 * Called when a {@link PubRelMessage} is received
+	 */
+	void handle(PubRelMessage message);
+
+	/**
+	 * Called when a {@link PubCompMessage} is received
+	 */
+	void handle(PubCompMessage message);
+
+	/**
+	 * Called when a {@link SubscribeMessage} is received
+	 */
+	void handle(SubscribeMessage message);
+
+	/**
+	 * Called when a {@link SubAckMessage} is received
+	 */
+	void handle(SubAckMessage message);
+
+	/**
+	 * Called when a {@link UnsubscribeMessage} is received
+	 */
+	void handle(UnsubscribeMessage message);
+
+	/**
+	 * Called when a {@link UnsubAckMessage} is received
+	 */
+	void handle(UnsubAckMessage message);
+
+	/**
+	 * Called when a {@link PingReqMessage} is received
+	 */
+	void handle(PingReqMessage message);
+
+	/**
+	 * Called when a {@link PingRespMessage} is received
+	 */
+	void handle(PingRespMessage message);
+
+	/**
+	 * Called when a {@link DisconnectMessage} is received
+	 */
+	void handle(DisconnectMessage message);
 }
--- trunk/src/main/java/net/sf/xenqtt/message/client/MqttChannel.java
+++ trunk/src/main/java/net/sf/xenqtt/message/client/MqttChannel.java
@@ -1,10 +1,31 @@
 package net.sf.xenqtt.message.client;
 
+import net.sf.xenqtt.message.MessageHandler;
 import net.sf.xenqtt.message.MqttMessage;
 
 /**
  * Sends and receives {@link MqttMessage}s over a channel. This may be client or server side.
  */
 public interface MqttChannel {
-	// FIXME [jim] - implement
+
+	/**
+	 * Reads data. This will read as many messages as it can and pass them to a {@link MessageHandler}.
+	 * 
+	 * @return True if the stream is still open. False if end of stream is reached.
+	 */
+	boolean read();
+
+	/**
+	 * Sends the specified message asynchronously.
+	 * 
+	 * @return True if this message is being sent immediately. False if there are other messages ahead of it.
+	 */
+	boolean send(MqttMessage message);
+
+	/**
+	 * Writes as much data as possible.
+	 * 
+	 * @return True if there is still pending data to send. False if there is none.
+	 */
+	boolean write();
 }
