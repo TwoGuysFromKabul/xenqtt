817cc65da2f044f2e6a40fc8bf6e6401c807bad1
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-08-17 02:03:30 +0000

    Minor refactoring. Added outline classes.

--- trunk/src/main/java/net/sf/xenqtt/gateway/GatewayServer.java
+++ trunk/src/main/java/net/sf/xenqtt/gateway/GatewayServer.java
@@ -1,7 +1,12 @@
 package net.sf.xenqtt.gateway;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //FIXME [jim] - for the gateway we want to always invoke all read ops as they will generate writes. then call all write ops
 public class GatewayServer {
+
+	private final Map<String, GatewayServer> serversByClientId = new HashMap<String, GatewayServer>();
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
