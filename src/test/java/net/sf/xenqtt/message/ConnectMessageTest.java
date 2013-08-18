bbddc2313e2954837943cece1471d54ee5b0155d
jorgamundus <jorgamundus@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-08-18 16:15:36 +0000

    Added tests for MqttMessage and ConnectMessage. Neither are done and there is a problem with ConnectMessage that the test has exposed that needs to be addressed.

--- src/test/java/net/sf/xenqtt/message/ConnectMessageTest.java
+++ src/test/java/net/sf/xenqtt/message/ConnectMessageTest.java
@@ -6,9 +6,16 @@ import org.junit.Test;
 
 public class ConnectMessageTest {
 
+	// TODO [jeremy] - There appears to be a problem in calculating the remaining length. This test precipitates a buffer overflow exception.
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInboundCtor() {
+		ConnectMessage message = new ConnectMessage("mr-burns", false, 1, "smithers", "hounds");
+
+		assertEquals("mr-burns", message.getClientId());
+		assertFalse(message.isCleanSession());
+		assertEquals(1, message.getKeepAliveSeconds());
+		assertEquals("smithers", message.getUserName());
+		assertEquals("hounds", message.getPassword());
 	}
 
 }
--- src/test/java/net/sf/xenqtt/message/MqttMessageTest.java
+++ src/test/java/net/sf/xenqtt/message/MqttMessageTest.java
@@ -7,8 +7,66 @@ import org.junit.Test;
 public class MqttMessageTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInboundCtor_MessageTypeAndRemainingLength() {
+		MqttMessage message = new MqttMessage(MessageType.CONNECT, 24);
+		message.buffer.flip();
+
+		assertEquals(MessageType.CONNECT, message.getMessageType());
+		assertEquals(24, message.getRemainingLength());
+		assertEquals(QoS.AT_MOST_ONCE, message.getQoS());
+	}
+
+	@Test
+	public void testInboundCtor_MessageTypeAndRemainingLengthAndFlags_FalseOnAllFlags() {
+		MqttMessage message = new MqttMessage(MessageType.CONNECT, false, QoS.EXACTLY_ONCE, false, 24);
+		message.buffer.flip();
+
+		assertEquals(MessageType.CONNECT, message.getMessageType());
+		assertEquals(24, message.getRemainingLength());
+		assertEquals(QoS.EXACTLY_ONCE, message.getQoS());
+		assertEquals(0, message.buffer.get(0) & 0x08);
+		assertEquals(0, message.buffer.get(0) & 0x01);
+	}
+
+	@Test
+	public void testInboundCtor_MessageTypeAndRemainingLengthAndFlags_Duplicate() {
+		MqttMessage message = new MqttMessage(MessageType.CONNECT, true, QoS.EXACTLY_ONCE, false, 24);
+		message.buffer.flip();
+
+		assertEquals(MessageType.CONNECT, message.getMessageType());
+		assertEquals(24, message.getRemainingLength());
+		assertEquals(QoS.EXACTLY_ONCE, message.getQoS());
+		assertEquals(0x08, message.buffer.get(0) & 0x08);
+		assertEquals(0, message.buffer.get(0) & 0x01);
+	}
+
+	@Test
+	public void testInboundCtor_MessageTypeAndRemainingLengthAndFlags_Retain() {
+		MqttMessage message = new MqttMessage(MessageType.CONNECT, false, QoS.EXACTLY_ONCE, true, 24);
+		message.buffer.flip();
+
+		assertEquals(MessageType.CONNECT, message.getMessageType());
+		assertEquals(24, message.getRemainingLength());
+		assertEquals(QoS.EXACTLY_ONCE, message.getQoS());
+		assertEquals(0, message.buffer.get(0) & 0x08);
+		assertEquals(0x01, message.buffer.get(0) & 0x01);
+	}
+
+	@Test
+	public void testInboundCtor_MessageTypeAndRemainingLengthAndFlags_AllFlags() {
+		MqttMessage message = new MqttMessage(MessageType.CONNECT, true, QoS.EXACTLY_ONCE, true, 24);
+		message.buffer.flip();
+
+		assertEquals(MessageType.CONNECT, message.getMessageType());
+		assertEquals(24, message.getRemainingLength());
+		assertEquals(QoS.EXACTLY_ONCE, message.getQoS());
+		assertEquals(0x08, message.buffer.get(0) & 0x08);
+		assertEquals(0x01, message.buffer.get(0) & 0x01);
+	}
+
+	@Test
+	public void testOutboundCtor() {
+		fail("Need to test this.");
 	}
 
 }
