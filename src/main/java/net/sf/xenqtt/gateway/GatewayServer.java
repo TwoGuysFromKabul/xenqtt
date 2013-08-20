d9e709ece09beee9bb47f5e003f0b0a497caa68e
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-08-20 21:39:51 +0000

    First implementation of GatewayServer. Not including unit tests.

--- src/main/java/net/sf/xenqtt/gateway/GatewayServer.java
+++ src/main/java/net/sf/xenqtt/gateway/GatewayServer.java
@@ -1,14 +1,202 @@
 package net.sf.xenqtt.gateway;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
-//FIXME [jim] - for the gateway we want to always invoke all read ops as they will generate writes. then call all write ops
+import net.sf.xenqtt.message.ConnAckMessage;
+import net.sf.xenqtt.message.ConnectMessage;
+import net.sf.xenqtt.message.DisconnectMessage;
+import net.sf.xenqtt.message.MessageHandler;
+import net.sf.xenqtt.message.MqttChannel;
+import net.sf.xenqtt.message.MqttChannelImpl;
+import net.sf.xenqtt.message.PingReqMessage;
+import net.sf.xenqtt.message.PingRespMessage;
+import net.sf.xenqtt.message.PubAckMessage;
+import net.sf.xenqtt.message.PubCompMessage;
+import net.sf.xenqtt.message.PubRecMessage;
+import net.sf.xenqtt.message.PubRelMessage;
+import net.sf.xenqtt.message.PublishMessage;
+import net.sf.xenqtt.message.SubAckMessage;
+import net.sf.xenqtt.message.SubscribeMessage;
+import net.sf.xenqtt.message.UnsubAckMessage;
+import net.sf.xenqtt.message.UnsubscribeMessage;
+
 public class GatewayServer {
 
-	private final Map<String, GatewayServer> serversByClientId = new HashMap<String, GatewayServer>();
+	// FIXME [jim] - maybe keep track of connections that don't send a connect message within some time period and close them?
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	private final Map<String, GatewaySession> sessionsByClientId = new HashMap<String, GatewaySession>();
+
+	private final Selector selector;
+	private final CountDownLatch exitLatch = new CountDownLatch(1);
+	private final ServerSocketChannel ssc;
+	private final MessageHandler handler = new Handler();
+
+	private volatile boolean running;
+
+	public static void main(String[] args) throws IOException {
+
+		final GatewayServer server = new GatewayServer();
+
+		Thread hook = new Thread() {
+			@Override
+			public void run() {
+				server.stop();
+			};
+		};
+
+		Runtime.getRuntime().addShutdownHook(hook);
+
+		server.run();
+	}
+
+	public GatewayServer() throws IOException {
+		this.selector = Selector.open();
+		this.ssc = ServerSocketChannel.open();
+	}
+
+	/**
+	 * Runs the server. The calling thread will not return until {@link #stop()} is called.
+	 */
+	public final void run() throws IOException {
+
+		ssc.configureBlocking(false);
+		ssc.socket().bind(new InetSocketAddress(9999));
+		SelectionKey serverKey = ssc.register(selector, SelectionKey.OP_ACCEPT);
+
+		running = true;
+
+		try {
+			while (running) {
+				selector.select();
+				Iterator<SelectionKey> keyIter = selector.selectedKeys().iterator();
+				while (keyIter.hasNext()) {
+					process(keyIter.next());
+					keyIter.remove();
+				}
+			}
+		} finally {
+			serverKey.cancel();
+			ssc.close();
+			exitLatch.countDown();
+		}
+	}
+
+	/**
+	 * Stops the server
+	 */
+	public final void stop() {
+
+		running = false;
+		selector.wakeup();
+		try {
+			exitLatch.await();
+		} catch (InterruptedException ignore) {
+		}
+	}
+
+	/**
+	 * Intended to be overridden to inject a mock sessions during unit testing
+	 */
+	GatewaySession createSession(MqttChannel channel, ConnectMessage message) {
+
+		return new GatewaySessionImpl(channel, message);
+	}
+
+	private void process(SelectionKey key) throws IOException {
+
+		if (!key.isValid()) {
+			return;
+		}
+
+		if (key.isAcceptable()) {
+			SocketChannel socketChannel = ssc.accept();
+			if (socketChannel != null) {
+				new MqttChannelImpl(socketChannel, handler, selector);
+			}
+
+		} else if (key.isReadable()) {
+			MqttChannel channel = (MqttChannel) key.attachment();
+			if (!channel.read()) {
+				channel.close();
+			}
+		}
+	}
+
+	private class Handler implements MessageHandler {
+
+		@Override
+		public void handle(MqttChannel channel, ConnectMessage message) {
+
+			channel.deregister();
+			String clientId = message.getClientId();
+			GatewaySession session = sessionsByClientId.get(clientId);
+			if (session == null) {
+				session = createSession(channel, message);
+				sessionsByClientId.put(clientId, session);
+			} else {
+				session.addClient(channel, message);
+			}
+		}
+
+		@Override
+		public void handle(MqttChannel channel, ConnAckMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, PublishMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, PubAckMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, PubRecMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, PubRelMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, PubCompMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, SubscribeMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, SubAckMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, UnsubscribeMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, UnsubAckMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, PingReqMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, PingRespMessage message) {
+		}
+
+		@Override
+		public void handle(MqttChannel channel, DisconnectMessage message) {
+		}
 	}
 }
--- src/main/java/net/sf/xenqtt/gateway/GatewaySessionImpl.java
+++ src/main/java/net/sf/xenqtt/gateway/GatewaySessionImpl.java
@@ -3,6 +3,9 @@ package net.sf.xenqtt.gateway;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.sf.xenqtt.message.ConnectMessage;
+import net.sf.xenqtt.message.MqttChannel;
+
 /**
  * The connections to the broker and all the clients for one client ID is a gateway session.
  */
@@ -11,11 +14,22 @@ public class GatewaySessionImpl extends Thread implements GatewaySession {
 	private static final List<ClientConnection> clients = new ArrayList<ClientConnection>();
 
 	/**
-	 * @param clientId
-	 *            The clientId for this session
+	 * @param channel
+	 *            The channel for the first client connection in the session
+	 * @param message
+	 *            The {@link ConnectMessage} received from the channel
 	 */
-	public GatewaySessionImpl(String clientId) {
-		super("GatewaySession-" + clientId);
+	public GatewaySessionImpl(MqttChannel channel, ConnectMessage message) {
+		super("GatewaySession-" + message.getClientId());
 		setDaemon(true);
+	}
+
+	/**
+	 * @see net.sf.xenqtt.gateway.GatewaySession#addClient(net.sf.xenqtt.message.MqttChannel, net.sf.xenqtt.message.ConnectMessage)
+	 */
+	@Override
+	public void addClient(MqttChannel channel, ConnectMessage message) {
+		// TODO Auto-generated method stub
+
 	}
 }
