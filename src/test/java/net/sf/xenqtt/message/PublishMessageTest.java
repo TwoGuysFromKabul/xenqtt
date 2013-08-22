8a3afda2b08537c0352e03189787f5b23e637c14
jorgamundus <jorgamundus@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-08-22 18:29:34 +0000

    Finished PublishMessage unit tests.

--- src/test/java/net/sf/xenqtt/message/PublishMessageTest.java
+++ src/test/java/net/sf/xenqtt/message/PublishMessageTest.java
@@ -2,13 +2,110 @@ package net.sf.xenqtt.message;
 
 import static org.junit.Assert.*;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
 import org.junit.Test;
 
 public class PublishMessageTest {
 
+	static final byte[] PAYLOAD = "To alcohol - the cause of, and solution to, all of life's problems".getBytes(Charset.forName("UTF-8"));
+	static final byte[] RECEIVED = new byte[] { 61, 90, 0, 20, 110, 101, 116, 46, 115, 102, 47, 109, 101, 115, 115, 97, 103, 101, 47, 116, 111, 112, 105, 99,
+			0, 1, 84, 111, 32, 97, 108, 99, 111, 104, 111, 108, 32, 45, 32, 116, 104, 101, 32, 99, 97, 117, 115, 101, 32, 111, 102, 44, 32, 97, 110, 100, 32,
+			115, 111, 108, 117, 116, 105, 111, 110, 32, 116, 111, 44, 32, 97, 108, 108, 32, 111, 102, 32, 108, 105, 102, 101, 39, 115, 32, 112, 114, 111, 98,
+			108, 101, 109, 115 };
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInboundCtor_NotDuplicateNotRetain() {
+		PublishMessage message = new PublishMessage(false, QoS.EXACTLY_ONCE, false, "net.sf/message/topic", 1, PAYLOAD);
+
+		assertSame(MessageType.PUBLISH, message.getMessageType());
+
+		assertEquals(1, message.getMessageId());
+		assertEquals("net.sf/message/topic", message.getTopicName());
+		assertEquals(QoS.EXACTLY_ONCE, message.getQoS());
+		assertEquals(2, message.getQoSLevel());
+		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertFalse(message.isDuplicate());
+		assertFalse(message.isRetain());
+	}
+
+	@Test
+	public void testInboundCtor_DuplicateNotRetain() {
+		PublishMessage message = new PublishMessage(true, QoS.EXACTLY_ONCE, false, "net.sf/message/topic", 1, PAYLOAD);
+
+		assertSame(MessageType.PUBLISH, message.getMessageType());
+
+		assertEquals(1, message.getMessageId());
+		assertEquals("net.sf/message/topic", message.getTopicName());
+		assertEquals(QoS.EXACTLY_ONCE, message.getQoS());
+		assertEquals(2, message.getQoSLevel());
+		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertTrue(message.isDuplicate());
+		assertFalse(message.isRetain());
+	}
+
+	@Test
+	public void testInboundCtor_NotDuplicateRetain() {
+		PublishMessage message = new PublishMessage(false, QoS.EXACTLY_ONCE, true, "net.sf/message/topic", 1, PAYLOAD);
+
+		assertSame(MessageType.PUBLISH, message.getMessageType());
+
+		assertEquals(1, message.getMessageId());
+		assertEquals("net.sf/message/topic", message.getTopicName());
+		assertEquals(QoS.EXACTLY_ONCE, message.getQoS());
+		assertEquals(2, message.getQoSLevel());
+		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertFalse(message.isDuplicate());
+		assertTrue(message.isRetain());
+	}
+
+	@Test
+	public void testInboundCtor_DuplicateRetain() {
+		PublishMessage message = new PublishMessage(true, QoS.EXACTLY_ONCE, true, "net.sf/message/topic", 1, PAYLOAD);
+
+		assertSame(MessageType.PUBLISH, message.getMessageType());
+
+		assertEquals(1, message.getMessageId());
+		assertEquals("net.sf/message/topic", message.getTopicName());
+		assertEquals(QoS.EXACTLY_ONCE, message.getQoS());
+		assertEquals(2, message.getQoSLevel());
+		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertTrue(message.isDuplicate());
+		assertTrue(message.isRetain());
+	}
+
+	@Test
+	public void testOutboundCtor() {
+		PublishMessage message = new PublishMessage(ByteBuffer.wrap(RECEIVED), 90);
+
+		assertSame(MessageType.PUBLISH, message.getMessageType());
+
+		assertEquals(1, message.getMessageId());
+		assertEquals("net.sf/message/topic", message.getTopicName());
+		assertEquals(QoS.EXACTLY_ONCE, message.getQoS());
+		assertEquals(2, message.getQoSLevel());
+		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertTrue(message.isDuplicate());
+		assertTrue(message.isRetain());
+	}
+
+	@Test
+	public void testSetMessageId() {
+		PublishMessage message = new PublishMessage(ByteBuffer.wrap(RECEIVED), 90);
+
+		assertSame(MessageType.PUBLISH, message.getMessageType());
+
+		assertEquals(1, message.getMessageId());
+		assertEquals("net.sf/message/topic", message.getTopicName());
+		assertEquals(QoS.EXACTLY_ONCE, message.getQoS());
+		assertEquals(2, message.getQoSLevel());
+		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertTrue(message.isDuplicate());
+		assertTrue(message.isRetain());
+
+		message.setMessageId(7);
+		assertEquals(7, message.getMessageId());
 	}
 
 }
