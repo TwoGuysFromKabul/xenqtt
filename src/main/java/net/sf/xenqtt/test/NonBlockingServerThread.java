f2faacb938ee7856ee6654ad4f1388020194285f
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-08-23 17:49:21 +0000

    improved non blocking tcp echo server

---
+++ src/main/java/net/sf/xenqtt/test/NonBlockingServerThread.java
@@ -0,0 +1,178 @@
+package net.sf.xenqtt.test;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public final class NonBlockingServerThread extends Thread {
+
+	private final int enableReadThreshold = (int) (NonBlockingTcpEchoServer.MAX_QUEUED_MSGS_PER_CONNECTION * .75);
+
+	private final BlockingQueue<SocketChannel> newClients = new LinkedBlockingQueue<SocketChannel>();
+
+	private final Selector selector;
+
+	public NonBlockingServerThread() throws IOException {
+		this.selector = Selector.open();
+	}
+
+	/**
+	 * @see java.lang.Thread#run()
+	 */
+	@Override
+	public void run() {
+		for (;;) {
+			try {
+				selector.select();
+				for (SocketChannel client = newClients.poll(); client != null; client = newClients.poll()) {
+					client.register(selector, SelectionKey.OP_READ, new ChannelInfo());
+				}
+
+				Set<SelectionKey> keys = selector.selectedKeys();
+				for (SelectionKey key : keys) {
+					handleKey(key);
+				}
+
+				keys.clear();
+			} catch (Exception e) {
+				System.err.println("IO Failure. Exiting...");
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+
+	public void newClient(SocketChannel channel) {
+		newClients.add(channel);
+		selector.wakeup();
+	}
+
+	private void handleKey(SelectionKey key) throws IOException {
+
+		if (!key.isValid()) {
+			return;
+		}
+
+		SocketChannel channel = (SocketChannel) key.channel();
+
+		try {
+
+			ChannelInfo info = (ChannelInfo) key.attachment();
+			if (key.isReadable()) {
+				read(channel, info, key);
+			}
+
+			if (key.isValid() && key.isWritable()) {
+				write(channel, info, key);
+			}
+
+		} catch (Exception e) {
+			if (channel.isOpen()) {
+				System.err.println("Error on channel. Closing...");
+				e.printStackTrace();
+				key.cancel();
+				key.attach(null);
+				channel.close();
+			}
+		}
+	}
+
+	private void write(SocketChannel channel, ChannelInfo info, SelectionKey key) throws IOException {
+
+		for (;;) {
+
+			if (info.currentSendMessage == null) {
+				disableOp(key, SelectionKey.OP_WRITE);
+				return;
+			}
+
+			channel.write(info.currentSendMessage);
+			if (info.currentSendMessage.hasRemaining()) {
+				return;
+			}
+
+			if (!isOpEnabled(key, SelectionKey.OP_READ) && info.sendQueue.size() <= enableReadThreshold) {
+				enableOp(key, SelectionKey.OP_READ);
+			}
+			info.currentSendMessage = info.sendQueue.poll();
+		}
+	}
+
+	private void read(SocketChannel channel, ChannelInfo info, SelectionKey key) throws IOException {
+
+		for (;;) {
+			if (!read(key, channel, info.receiveHeader)) {
+				return;
+			}
+
+			if (info.receivePayload == null) {
+				int len = info.receiveHeader.getShort(0) & 0xffff;
+				info.receivePayload = ByteBuffer.allocate(len + 2);
+				info.receivePayload.putShort((short) len);
+			}
+
+			if (!read(key, channel, info.receivePayload)) {
+				return;
+			}
+
+			info.receivePayload.flip();
+
+			if (info.currentSendMessage == null) {
+				info.currentSendMessage = info.receivePayload;
+				enableOp(key, SelectionKey.OP_WRITE);
+			} else {
+				info.sendQueue.add(info.receivePayload);
+				if (info.sendQueue.size() == NonBlockingTcpEchoServer.MAX_QUEUED_MSGS_PER_CONNECTION) {
+					disableOp(key, SelectionKey.OP_READ);
+				}
+			}
+
+			info.receiveHeader.clear();
+			info.receivePayload = null;
+		}
+	}
+
+	private boolean read(SelectionKey key, SocketChannel channel, ByteBuffer buffer) throws IOException {
+
+		if (!buffer.hasRemaining()) {
+			return true;
+		}
+
+		if (channel.read(buffer) < 0) {
+			key.cancel();
+			key.attach(null);
+			channel.close();
+
+			return false;
+		}
+
+		return !buffer.hasRemaining();
+	}
+
+	private boolean isOpEnabled(SelectionKey key, int op) {
+		return (key.interestOps() & op) == op;
+	}
+
+	private void enableOp(SelectionKey key, int op) {
+		key.interestOps(key.interestOps() | op);
+	}
+
+	private void disableOp(SelectionKey key, int op) {
+		key.interestOps(key.interestOps() & (~op));
+	}
+
+	private static final class ChannelInfo {
+
+		final Queue<ByteBuffer> sendQueue = new ArrayDeque<ByteBuffer>(NonBlockingTcpEchoServer.MAX_QUEUED_MSGS_PER_CONNECTION);
+		final ByteBuffer receiveHeader = ByteBuffer.allocate(2);
+		ByteBuffer receivePayload;
+		ByteBuffer currentSendMessage;
+	}
+}
--- src/main/java/net/sf/xenqtt/test/NonBlockingTcpEchoServer.java
+++ src/main/java/net/sf/xenqtt/test/NonBlockingTcpEchoServer.java
@@ -2,14 +2,8 @@ package net.sf.xenqtt.test;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Set;
 
 /**
  * Server that uses non-blocking IO to accept new connections and echo any data received back to the sender. The data must be in this format:
@@ -20,9 +14,12 @@ import java.util.Set;
  */
 public class NonBlockingTcpEchoServer {
 
-	private final Selector selector;
-	private final ServerSocketChannel ssc;
+	static final int MAX_QUEUED_MSGS_PER_CONNECTION = 1000;
+
+	private final NonBlockingServerThread[] serverThreads;
 	private final int port;
+
+	private int nextIndex;
 
 	public static void main(String[] args) throws Exception {
 
@@ -38,9 +35,9 @@ public class NonBlockingTcpEchoServer {
 
 	private NonBlockingTcpEchoServer(int port) throws IOException {
 
-		this.selector = Selector.open();
-		this.ssc = ServerSocketChannel.open();
 		this.port = port;
+		int threadCount = Runtime.getRuntime().availableProcessors();
+		this.serverThreads = new NonBlockingServerThread[threadCount];
 	}
 
 	private static void usage() {
@@ -51,119 +48,27 @@ public class NonBlockingTcpEchoServer {
 
 	private void run() throws IOException {
 
-		ssc.configureBlocking(false);
+		for (int i = 0; i < serverThreads.length; i++) {
+			serverThreads[i] = new NonBlockingServerThread();
+			serverThreads[i].setName("Server-" + i);
+			serverThreads[i].setDaemon(true);
+			serverThreads[i].start();
+		}
+
+		ServerSocketChannel ssc = ServerSocketChannel.open();
 		ssc.socket().bind(new InetSocketAddress(port));
-		ssc.register(selector, SelectionKey.OP_ACCEPT);
 
 		System.out.println("Listening on port " + port);
 		System.out.println("Ctrl-C to exit...\n");
 
 		for (;;) {
-
-			selector.select();
-
-			Set<SelectionKey> keys = selector.selectedKeys();
-			for (SelectionKey key : keys) {
-				handleKey(key);
+			SocketChannel channel = ssc.accept();
+			channel.configureBlocking(false);
+			serverThreads[nextIndex++].newClient(channel);
+			if (nextIndex == serverThreads.length) {
+				nextIndex = 0;
 			}
-
-			keys.clear();
-		}
-	}
-
-	private void handleKey(SelectionKey key) throws IOException {
-
-		if (!key.isValid()) {
-			return;
 		}
 
-		if (key.isAcceptable()) {
-			SocketChannel newChannel = ssc.accept();
-			newChannel.configureBlocking(false);
-			SelectionKey newKey = newChannel.register(selector, SelectionKey.OP_READ);
-			newKey.attach(new ChannelInfo());
-			return;
-		}
-
-		SocketChannel channel = (SocketChannel) key.channel();
-		ChannelInfo info = (ChannelInfo) key.attachment();
-		if (key.isReadable()) {
-			read(channel, info, key);
-		}
-
-		if (key.isValid() && key.isWritable()) {
-			write(channel, info, key);
-		}
-	}
-
-	private void write(SocketChannel channel, ChannelInfo info, SelectionKey key) throws IOException {
-
-		for (;;) {
-
-			if (info.currentSendMessage == null) {
-				key.interestOps(SelectionKey.OP_READ);
-				return;
-			}
-
-			channel.write(info.currentSendMessage);
-			if (info.currentSendMessage.hasRemaining()) {
-				return;
-			}
-
-			info.currentSendMessage = info.sendQueue.poll();
-		}
-	}
-
-	private void read(SocketChannel channel, ChannelInfo info, SelectionKey key) throws IOException {
-
-		for (;;) {
-			if (!read(channel, info.receiveHeader)) {
-				return;
-			}
-
-			if (info.receivePayload == null) {
-				int len = info.receiveHeader.getShort(0) & 0xffff;
-				info.receivePayload = ByteBuffer.allocate(len + 2);
-				info.receivePayload.putShort((short) len);
-			}
-
-			if (!read(channel, info.receivePayload)) {
-				return;
-			}
-
-			info.receivePayload.flip();
-
-			if (info.currentSendMessage == null) {
-				info.currentSendMessage = info.receivePayload;
-				key.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_READ);
-			} else {
-				info.sendQueue.add(info.receivePayload);
-			}
-
-			info.receiveHeader.clear();
-			info.receivePayload = null;
-		}
-	}
-
-	private boolean read(SocketChannel channel, ByteBuffer buffer) throws IOException {
-
-		if (!buffer.hasRemaining()) {
-			return true;
-		}
-
-		if (channel.read(buffer) < 0) {
-			channel.close();
-			return false;
-		}
-
-		return !buffer.hasRemaining();
-	}
-
-	private static final class ChannelInfo {
-
-		final Queue<ByteBuffer> sendQueue = new ArrayDeque<ByteBuffer>();
-		final ByteBuffer receiveHeader = ByteBuffer.allocate(2);
-		ByteBuffer receivePayload;
-		ByteBuffer currentSendMessage;
 	}
 }
