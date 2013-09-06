e934e8a21c221b0e5470616d751332f6fd801a08
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-09-06 18:03:22 +0000

    Updated javadoc

--- src/main/java/net/sf/xenqtt/client/PublishListener.java
+++ src/main/java/net/sf/xenqtt/client/PublishListener.java
@@ -1,9 +1,21 @@
 package net.sf.xenqtt.client;
 
+import net.sf.xenqtt.message.QoS;
+
 /**
- * Specifies a type that handles publishing messages to an MQTT broker.
+ * Implement this interface to use {@link SynchronousMqttClient}. The client will invoke the methods in this interface when a published message is received. A
+ * single instance of this interface may be used with multiple clients.
  */
 public interface PublishListener {
 
+	/**
+	 * Called when a published message is received from the broker. You should always call {@link PublishMessage#ack() ack()} when you are done processing the
+	 * message. This is not required if the {@link PublishMessage#getQoS() QoS} is {@link QoS#AT_MOST_ONCE} but it is a good practice to always call it.
+	 * 
+	 * @param client
+	 *            The client that received the message
+	 * @param message
+	 *            The message that was published
+	 */
 	void publish(MqttClient client, PublishMessage message);
 }
