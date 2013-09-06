ee1be8397ba9e96228ec2e9fd4cbe38a039e42ee
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-09-06 14:47:11 +0000

    Added MqttTimeoutException

---
+++ src/main/java/net/sf/xenqtt/MqttTimeoutException.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package net.sf.xenqtt;
+
+/**
+ * Thrown when a timeout occurs in xenqtt.
+ */
+public class MqttTimeoutException extends MqttException {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructs a new mqtt exception with <code>null</code> as its detail message. The cause is not initialized, and may subsequently be initialized by a call
+	 * to {@link #initCause}.
+	 */
+	public MqttTimeoutException() {
+	}
+
+	/**
+	 * Constructs a new mqtt exception with the specified detail message and cause.
+	 * <p>
+	 * Note that the detail message associated with <code>cause</code> is <i>not</i> automatically incorporated in this mqtt exception's detail message.
+	 * 
+	 * @param message
+	 *            the detail message (which is saved for later retrieval by the {@link #getMessage()} method).
+	 * @param cause
+	 *            the cause (which is saved for later retrieval by the {@link #getCause()} method). (A <tt>null</tt> value is permitted, and indicates that the
+	 *            cause is nonexistent or unknown.)
+	 */
+	public MqttTimeoutException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * Constructs a new mqtt exception with the specified detail message. The cause is not initialized, and may subsequently be initialized by a call to
+	 * {@link #initCause}.
+	 * 
+	 * @param message
+	 *            the detail message. The detail message is saved for later retrieval by the {@link #getMessage()} method.
+	 */
+	public MqttTimeoutException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructs a new mqtt exception with the specified cause and a detail message of <tt>(cause==null ? null : cause.toString())</tt> (which typically
+	 * contains the class and detail message of <tt>cause</tt>). This constructor is useful for mqtt exceptions that are little more than wrappers for other
+	 * throwables.
+	 * 
+	 * @param cause
+	 *            the cause (which is saved for later retrieval by the {@link #getCause()} method). (A <tt>null</tt> value is permitted, and indicates that the
+	 *            cause is nonexistent or unknown.)
+	 * @since 1.4
+	 */
+	public MqttTimeoutException(Throwable cause) {
+		super(cause);
+	}
+}
