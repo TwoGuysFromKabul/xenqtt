f186dc615f6545943f49c290d601c445a33188b2
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-09-07 00:58:54 +0000

    changed published message payload as string encoding to utf8 and created PublishMessageImpl.

---
+++ src/test/java/net/sf/xenqtt/client/PublishMessageImplTest.java
@@ -0,0 +1,112 @@
+package net.sf.xenqtt.client;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+import net.sf.xenqtt.ChannelManager;
+import net.sf.xenqtt.message.MqttChannel;
+import net.sf.xenqtt.message.MqttChannelRef;
+import net.sf.xenqtt.message.MqttMessage;
+import net.sf.xenqtt.message.PubAckMessage;
+import net.sf.xenqtt.message.PubMessage;
+import net.sf.xenqtt.message.QoS;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * FIXME [jim] - needs javadoc
+ */
+public class PublishMessageImplTest {
+
+	@Mock ChannelManager channelManager;
+	@Mock MqttChannelRef channel;
+	PubMessage pubMessage = new PubMessage(QoS.AT_LEAST_ONCE, false, "my topic", 123, new byte[] { 97, 98, 99 });
+	PublishMessage message;
+
+	@Before
+	public void before() {
+		MockitoAnnotations.initMocks(this);
+		message = new PublishMessageImpl(channelManager, channel, pubMessage);
+	}
+
+	@Test
+	public void testGetTopic() throws Exception {
+		assertEquals("my topic", message.getTopic());
+	}
+
+	@Test
+	public void testGetPayload() throws Exception {
+
+		assertArrayEquals(new byte[] { 97, 98, 99 }, message.getPayload());
+	}
+
+	@Test
+	public void testGetPayloadString() throws Exception {
+
+		assertEquals("abc", message.getPayloadString());
+	}
+
+	@Test
+	public void testIsRetain() throws Exception {
+
+		assertFalse(message.isRetain());
+
+		pubMessage = new PubMessage(QoS.AT_LEAST_ONCE, true, "my topic", 123, new byte[] { 97, 98, 99 });
+		message = new PublishMessageImpl(channelManager, channel, pubMessage);
+
+		assertTrue(message.isRetain());
+	}
+
+	@Test
+	public void testIsDuplicate() throws Exception {
+
+		assertFalse(message.isDuplicate());
+
+		pubMessage.setDuplicateFlag();
+
+		assertTrue(message.isDuplicate());
+	}
+
+	@Test
+	public void testGetQos() throws Exception {
+
+		assertEquals(QoS.AT_LEAST_ONCE, message.getQoS());
+
+		pubMessage = new PubMessage(QoS.AT_MOST_ONCE, false, "my topic", 123, new byte[] { 97, 98, 99 });
+		message = new PublishMessageImpl(channelManager, channel, pubMessage);
+
+		assertEquals(QoS.AT_MOST_ONCE, message.getQoS());
+	}
+
+	@Test
+	public void testAck_Qos0() throws Exception {
+
+		pubMessage = new PubMessage(QoS.AT_MOST_ONCE, false, "my topic", 123, new byte[] { 97, 98, 99 });
+		message = new PublishMessageImpl(channelManager, channel, pubMessage);
+
+		message.ack();
+		verifyZeroInteractions(channelManager);
+		assertFalse(Thread.interrupted());
+	}
+
+	@Test
+	public void testAck_Qos1() throws Exception {
+
+		message.ack();
+		verify(channelManager).send(channel, new PubAckMessage(123));
+		assertFalse(Thread.interrupted());
+	}
+
+	@Test
+	public void testAck_Interrupted() throws Exception {
+
+		when(channelManager.send(any(MqttChannel.class), any(MqttMessage.class))).thenThrow(new InterruptedException());
+
+		message.ack();
+
+		assertTrue(Thread.interrupted());
+	}
+}
