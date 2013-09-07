fdef6186e6d8de9cb08a475e161694aa9432f06a
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-09-07 12:39:39 +0000

    Removed unnecessary constructor from MqttInterruptedException

--- src/main/java/net/sf/xenqtt/MqttInterruptedException.java
+++ src/main/java/net/sf/xenqtt/MqttInterruptedException.java
@@ -12,20 +12,6 @@ public class MqttInterruptedException extends MqttException {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Constructs a new mqtt exception with the specified detail message and cause.
-	 * <p>
-	 * Note that the detail message associated with <code>cause</code> is <i>not</i> automatically incorporated in this mqtt exception's detail message.
-	 * 
-	 * @param message
-	 *            the detail message (which is saved for later retrieval by the {@link #getMessage()} method).
-	 * @param cause
-	 *            the cause (which is saved for later retrieval by the {@link #getCause()} method).
-	 */
-	public MqttInterruptedException(String message, InterruptedException cause) {
-		super(message, cause);
-	}
-
-	/**
 	 * Constructs a new mqtt exception with the specified cause and a detail message of <tt>(cause==null ? null : cause.toString())</tt> (which typically
 	 * contains the class and detail message of <tt>cause</tt>).
 	 * 
