1faa8d88e527a9d48abea0ab0fa0d1472e5f363e
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-09-09 12:56:06 +0000

    Fixed a bug in PubMessage where the message ID was included when the qos == 0

--- src/test/java/net/sf/xenqtt/message/PubMessageTest.java
+++ src/test/java/net/sf/xenqtt/message/PubMessageTest.java
@@ -9,15 +9,19 @@ import org.junit.Test;
 
 public class PubMessageTest {
 
-	static final byte[] PAYLOAD = "To alcohol - the cause of, and solution to, all of life's problems".getBytes(Charset.forName("UTF-8"));
-	static final byte[] RECEIVED = new byte[] { 59, 90, 0, 20, 110, 101, 116, 46, 115, 102, 47, 109, 101, 115, 115, 97, 103, 101, 47, 116, 111, 112, 105, 99,
-			0, 1, 84, 111, 32, 97, 108, 99, 111, 104, 111, 108, 32, 45, 32, 116, 104, 101, 32, 99, 97, 117, 115, 101, 32, 111, 102, 44, 32, 97, 110, 100, 32,
-			115, 111, 108, 117, 116, 105, 111, 110, 32, 116, 111, 44, 32, 97, 108, 108, 32, 111, 102, 32, 108, 105, 102, 101, 39, 115, 32, 112, 114, 111, 98,
-			108, 101, 109, 115 };
+	final byte[] paylaod = "To alcohol - the cause of, and solution to, all of life's problems".getBytes(Charset.forName("UTF-8"));
+	final byte[] qos1Bytes = new byte[] { 59, 90, 0, 20, 110, 101, 116, 46, 115, 102, 47, 109, 101, 115, 115, 97, 103, 101, 47, 116, 111, 112, 105, 99, 0, 1,
+			84, 111, 32, 97, 108, 99, 111, 104, 111, 108, 32, 45, 32, 116, 104, 101, 32, 99, 97, 117, 115, 101, 32, 111, 102, 44, 32, 97, 110, 100, 32, 115,
+			111, 108, 117, 116, 105, 111, 110, 32, 116, 111, 44, 32, 97, 108, 108, 32, 111, 102, 32, 108, 105, 102, 101, 39, 115, 32, 112, 114, 111, 98, 108,
+			101, 109, 115 };
+	final byte[] qos0Bytes = new byte[] { 57, 88, 0, 20, 110, 101, 116, 46, 115, 102, 47, 109, 101, 115, 115, 97, 103, 101, 47, 116, 111, 112, 105, 99, 84,
+			111, 32, 97, 108, 99, 111, 104, 111, 108, 32, 45, 32, 116, 104, 101, 32, 99, 97, 117, 115, 101, 32, 111, 102, 44, 32, 97, 110, 100, 32, 115, 111,
+			108, 117, 116, 105, 111, 110, 32, 116, 111, 44, 32, 97, 108, 108, 32, 111, 102, 32, 108, 105, 102, 101, 39, 115, 32, 112, 114, 111, 98, 108, 101,
+			109, 115 };
 
 	@Test
-	public void testInboundCtor_NotDuplicateNotRetain() {
-		PubMessage message = new PubMessage(QoS.AT_LEAST_ONCE, false, "net.sf/message/topic", 1, PAYLOAD);
+	public void testOuboundCtor_Qos1_NotDuplicateNotRetain() {
+		PubMessage message = new PubMessage(QoS.AT_LEAST_ONCE, false, "net.sf/message/topic", 1, paylaod);
 
 		assertSame(MessageType.PUBLISH, message.getMessageType());
 
@@ -25,14 +29,16 @@ public class PubMessageTest {
 		assertEquals("net.sf/message/topic", message.getTopicName());
 		assertEquals(QoS.AT_LEAST_ONCE, message.getQoS());
 		assertEquals(1, message.getQoSLevel());
-		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertArrayEquals(paylaod, message.getPayload());
 		assertFalse(message.isDuplicate());
 		assertFalse(message.isRetain());
+		qos1Bytes[0] -= 9;
+		assertArrayEquals(qos1Bytes, message.buffer.array());
 	}
 
 	@Test
-	public void testInboundCtor_DuplicateNotRetain() {
-		PubMessage message = new PubMessage(QoS.AT_LEAST_ONCE, false, "net.sf/message/topic", 1, PAYLOAD);
+	public void testOuboundCtor_Qos1_DuplicateNotRetain() {
+		PubMessage message = new PubMessage(QoS.AT_LEAST_ONCE, false, "net.sf/message/topic", 1, paylaod);
 		message.setDuplicateFlag();
 
 		assertSame(MessageType.PUBLISH, message.getMessageType());
@@ -41,14 +47,18 @@ public class PubMessageTest {
 		assertEquals("net.sf/message/topic", message.getTopicName());
 		assertEquals(QoS.AT_LEAST_ONCE, message.getQoS());
 		assertEquals(1, message.getQoSLevel());
-		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertArrayEquals(paylaod, message.getPayload());
 		assertTrue(message.isDuplicate());
 		assertFalse(message.isRetain());
+		qos1Bytes[0] -= 1;
+		byte[] bytes = new byte[qos1Bytes.length];
+		message.buffer.get(bytes);
+		assertArrayEquals(qos1Bytes, bytes);
 	}
 
 	@Test
-	public void testInboundCtor_NotDuplicateRetain() {
-		PubMessage message = new PubMessage(QoS.AT_LEAST_ONCE, true, "net.sf/message/topic", 1, PAYLOAD);
+	public void testOuboundCtor_Qos1_NotDuplicateRetain() {
+		PubMessage message = new PubMessage(QoS.AT_LEAST_ONCE, true, "net.sf/message/topic", 1, paylaod);
 
 		assertSame(MessageType.PUBLISH, message.getMessageType());
 
@@ -56,14 +66,18 @@ public class PubMessageTest {
 		assertEquals("net.sf/message/topic", message.getTopicName());
 		assertEquals(QoS.AT_LEAST_ONCE, message.getQoS());
 		assertEquals(1, message.getQoSLevel());
-		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertArrayEquals(paylaod, message.getPayload());
 		assertFalse(message.isDuplicate());
 		assertTrue(message.isRetain());
+		qos1Bytes[0] -= 8;
+		byte[] bytes = new byte[qos1Bytes.length];
+		message.buffer.get(bytes);
+		assertArrayEquals(qos1Bytes, bytes);
 	}
 
 	@Test
-	public void testInboundCtor_DuplicateRetain() {
-		PubMessage message = new PubMessage(QoS.AT_LEAST_ONCE, true, "net.sf/message/topic", 1, PAYLOAD);
+	public void testOuboundCtor_Qos1_DuplicateRetain() {
+		PubMessage message = new PubMessage(QoS.AT_LEAST_ONCE, true, "net.sf/message/topic", 1, paylaod);
 		message.setDuplicateFlag();
 
 		assertSame(MessageType.PUBLISH, message.getMessageType());
@@ -72,14 +86,17 @@ public class PubMessageTest {
 		assertEquals("net.sf/message/topic", message.getTopicName());
 		assertEquals(QoS.AT_LEAST_ONCE, message.getQoS());
 		assertEquals(1, message.getQoSLevel());
-		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertArrayEquals(paylaod, message.getPayload());
 		assertTrue(message.isDuplicate());
 		assertTrue(message.isRetain());
+		byte[] bytes = new byte[qos1Bytes.length];
+		message.buffer.get(bytes);
+		assertArrayEquals(qos1Bytes, bytes);
 	}
 
 	@Test
-	public void testOutboundCtor() {
-		PubMessage message = new PubMessage(ByteBuffer.wrap(RECEIVED), 90);
+	public void testInboundCtor_Qos1() {
+		PubMessage message = new PubMessage(ByteBuffer.wrap(qos1Bytes), 90);
 
 		assertSame(MessageType.PUBLISH, message.getMessageType());
 
@@ -87,14 +104,14 @@ public class PubMessageTest {
 		assertEquals("net.sf/message/topic", message.getTopicName());
 		assertEquals(QoS.AT_LEAST_ONCE, message.getQoS());
 		assertEquals(1, message.getQoSLevel());
-		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertArrayEquals(paylaod, message.getPayload());
 		assertTrue(message.isDuplicate());
 		assertTrue(message.isRetain());
 	}
 
 	@Test
-	public void testSetMessageId() {
-		PubMessage message = new PubMessage(ByteBuffer.wrap(RECEIVED), 90);
+	public void testSetMessageId_Qos1() {
+		PubMessage message = new PubMessage(ByteBuffer.wrap(qos1Bytes), 90);
 
 		assertSame(MessageType.PUBLISH, message.getMessageType());
 
@@ -102,7 +119,7 @@ public class PubMessageTest {
 		assertEquals("net.sf/message/topic", message.getTopicName());
 		assertEquals(QoS.AT_LEAST_ONCE, message.getQoS());
 		assertEquals(1, message.getQoSLevel());
-		assertArrayEquals(PAYLOAD, message.getPayload());
+		assertArrayEquals(paylaod, message.getPayload());
 		assertTrue(message.isDuplicate());
 		assertTrue(message.isRetain());
 
@@ -110,4 +127,102 @@ public class PubMessageTest {
 		assertEquals(7, message.getMessageId());
 	}
 
+	@Test
+	public void testOuboundCtor_Qos0_NotDuplicateNotRetain() {
+		PubMessage message = new PubMessage(QoS.AT_MOST_ONCE, false, "net.sf/message/topic", 1, paylaod);
+
+		assertSame(MessageType.PUBLISH, message.getMessageType());
+
+		assertEquals(0, message.getMessageId());
+		assertEquals("net.sf/message/topic", message.getTopicName());
+		assertEquals(QoS.AT_MOST_ONCE, message.getQoS());
+		assertEquals(0, message.getQoSLevel());
+		assertArrayEquals(paylaod, message.getPayload());
+		assertFalse(message.isDuplicate());
+		assertFalse(message.isRetain());
+		qos0Bytes[0] -= 9;
+		byte[] bytes = new byte[qos0Bytes.length];
+		message.buffer.get(bytes);
+		assertArrayEquals(qos0Bytes, bytes);
+	}
+
+	@Test
+	public void testOuboundCtor_Qos0_DuplicateNotRetain() {
+		PubMessage message = new PubMessage(QoS.AT_MOST_ONCE, false, "net.sf/message/topic", 1, paylaod);
+		message.setDuplicateFlag();
+
+		assertSame(MessageType.PUBLISH, message.getMessageType());
+
+		assertEquals(0, message.getMessageId());
+		assertEquals("net.sf/message/topic", message.getTopicName());
+		assertEquals(QoS.AT_MOST_ONCE, message.getQoS());
+		assertEquals(0, message.getQoSLevel());
+		assertArrayEquals(paylaod, message.getPayload());
+		assertTrue(message.isDuplicate());
+		assertFalse(message.isRetain());
+		qos0Bytes[0] -= 1;
+		byte[] bytes = new byte[qos0Bytes.length];
+		message.buffer.get(bytes);
+		assertArrayEquals(qos0Bytes, bytes);
+	}
+
+	@Test
+	public void testOuboundCtor_Qos0_NotDuplicateRetain() {
+		PubMessage message = new PubMessage(QoS.AT_MOST_ONCE, true, "net.sf/message/topic", 1, paylaod);
+
+		assertSame(MessageType.PUBLISH, message.getMessageType());
+
+		assertEquals(0, message.getMessageId());
+		assertEquals("net.sf/message/topic", message.getTopicName());
+		assertEquals(QoS.AT_MOST_ONCE, message.getQoS());
+		assertEquals(0, message.getQoSLevel());
+		assertArrayEquals(paylaod, message.getPayload());
+		assertFalse(message.isDuplicate());
+		assertTrue(message.isRetain());
+		qos0Bytes[0] -= 8;
+		byte[] bytes = new byte[qos0Bytes.length];
+		message.buffer.get(bytes);
+		assertArrayEquals(qos0Bytes, bytes);
+	}
+
+	@Test
+	public void testOuboundCtor_Qos0_DuplicateRetain() {
+		PubMessage message = new PubMessage(QoS.AT_MOST_ONCE, true, "net.sf/message/topic", 1, paylaod);
+		message.setDuplicateFlag();
+
+		assertSame(MessageType.PUBLISH, message.getMessageType());
+
+		assertEquals(0, message.getMessageId());
+		assertEquals("net.sf/message/topic", message.getTopicName());
+		assertEquals(QoS.AT_MOST_ONCE, message.getQoS());
+		assertEquals(0, message.getQoSLevel());
+		assertArrayEquals(paylaod, message.getPayload());
+		assertTrue(message.isDuplicate());
+		assertTrue(message.isRetain());
+		byte[] bytes = new byte[qos0Bytes.length];
+		message.buffer.get(bytes);
+		assertArrayEquals(qos0Bytes, bytes);
+	}
+
+	@Test
+	public void testInboundCtor_Qos0() {
+		PubMessage message = new PubMessage(ByteBuffer.wrap(qos0Bytes), 90);
+
+		assertSame(MessageType.PUBLISH, message.getMessageType());
+
+		assertEquals(0, message.getMessageId());
+		assertEquals("net.sf/message/topic", message.getTopicName());
+		assertEquals(QoS.AT_MOST_ONCE, message.getQoS());
+		assertEquals(0, message.getQoSLevel());
+		assertArrayEquals(paylaod, message.getPayload());
+		assertTrue(message.isDuplicate());
+		assertTrue(message.isRetain());
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void testSetMessageId_Qos0() {
+
+		PubMessage message = new PubMessage(ByteBuffer.wrap(qos0Bytes), 90);
+		message.setMessageId(7);
+	}
 }
