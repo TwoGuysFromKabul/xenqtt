04c177fef74c52259b639c58967b28a7cf0fe0f3
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-09-10 17:26:55 +0000

    Added a constructor to MqttCommandCancelledException

--- trunk/src/main/java/net/sf/xenqtt/MqttCommandCancelledException.java
+++ trunk/src/main/java/net/sf/xenqtt/MqttCommandCancelledException.java
@@ -29,4 +29,19 @@ public class MqttCommandCancelledException extends MqttException {
 	public MqttCommandCancelledException(String message) {
 		super(message);
 	}
+
+	/**
+	 * Constructs a new instance with the specified detail message and cause.
+	 * <p>
+	 * Note that the detail message associated with <code>cause</code> is <i>not</i> automatically incorporated in thisinstance's detail message.
+	 * 
+	 * @param message
+	 *            the detail message (which is saved for later retrieval by the {@link #getMessage()} method).
+	 * @param cause
+	 *            the cause (which is saved for later retrieval by the {@link #getCause()} method). (A <tt>null</tt> value is permitted, and indicates that the
+	 *            cause is nonexistent or unknown.)
+	 */
+	public MqttCommandCancelledException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
