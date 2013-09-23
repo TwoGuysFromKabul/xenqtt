3806eb2257db6b8f84c43b6e8bfc5438ee71a2f4
jorgamundus <jorgamundus@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-09-23 16:57:15 +0000

    Added argument validation methods.

--- trunk/src/main/java/net/sf/xenqtt/XenqttUtil.java
+++ trunk/src/main/java/net/sf/xenqtt/XenqttUtil.java
@@ -1,5 +1,7 @@
 package net.sf.xenqtt;
 
+import java.util.Collection;
+
 /**
  * Provides disparate utility methods useful across the Xenqtt application ecosystem.
  */
@@ -84,6 +86,235 @@ public final class XenqttUtil {
 		}
 
 		return count;
+	}
+
+	/**
+	 * Validates that an object is not {@code null}.
+	 * 
+	 * @param name
+	 *            The name of the object being validated. Included in the exception message if one is thrown
+	 * @param value
+	 *            The object to validate
+	 * 
+	 * @return The specified {@code value}
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If {@code value} is {@code null}
+	 */
+	public static <T> T validateNotNull(String name, T value) {
+		if (value == null) {
+			doThrow("The argument %s must not be null.", name);
+		}
+
+		return value;
+	}
+
+	/**
+	 * Validate that a {@link String string} is not empty. An empty string is either {@code null} or the empty string. Having only whitespace in the string (
+	 * {@code \r \n \t ' '}) constitutes an empty string as well.
+	 * 
+	 * @param name
+	 *            The name of the object being validated. Included in the exception message if one is thrown
+	 * @param value
+	 *            The string to validate
+	 * 
+	 * @return The specified {@code value}
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If {@code value} is an empty string
+	 */
+	public static String validateNotEmpty(String name, String value) {
+		if (value == null || value.trim().equals("")) {
+			doThrow("The argument %s must not be empty.", name);
+		}
+
+		return value;
+	}
+
+	/**
+	 * Validate that an arbitrary array of objects of type {@code T} is not empty. The array is considered empty if it is {@code null} or contains zero
+	 * elements.
+	 * 
+	 * @param name
+	 *            The name of the object being validated. Included in the exception message if one is thrown
+	 * @param value
+	 *            The array to validate
+	 * 
+	 * @return The specified {@code value}
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If {@code value} is {@code null} or contains no elements
+	 */
+	public static <T> T[] validateNotEmpty(String name, T[] value) {
+		if (value == null || value.length == 0) {
+			doThrow("The argument %s must not be empty.", name);
+		}
+
+		return value;
+	}
+
+	/**
+	 * Validate that an arbitrary {@link Collection collection} of objects of type {@code T} is not empty. The collection is considered empty if it is
+	 * {@code null} or contains zero elements.
+	 * 
+	 * @param name
+	 *            The name of the object being validated. Included in the exception message if one is thrown
+	 * @param value
+	 *            The collection to validate
+	 * 
+	 * @return The specified {@code value}
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If {@code value} is {@code null} or contains no elements
+	 */
+	public static <T> Collection<T> validateNotEmpty(String name, Collection<T> value) {
+		if (value == null || value.size() == 0) {
+			doThrow("The argument %s must not be empty.", name);
+		}
+
+		return value;
+	}
+
+	/**
+	 * Validate that a number is less than a defined maximum.
+	 * 
+	 * @param name
+	 *            The name of the object being validated. Included in the exception message if one is thrown
+	 * @param value
+	 *            The value of the number
+	 * @param max
+	 *            The maximum that the specified {@code value} must be under
+	 * 
+	 * @return The specified {@code value}
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If {@code value} is {@code null} or is greater than or equal to {@code max}
+	 */
+	public static <T extends Number> T validateLessThan(String name, T value, T max) {
+		if (value == null) {
+			doThrow("The argument %s must not be null.", name);
+		}
+
+		if (value.longValue() >= max.longValue()) {
+			doThrow("The argument %s must not be greater than or equal to %d. Was: %d", name, value, max);
+		}
+
+		return value;
+	}
+
+	/**
+	 * Validate that a number is less than or equal to a defined maximum.
+	 * 
+	 * @param name
+	 *            The name of the object being validated. Included in the exception message if one is thrown
+	 * @param value
+	 *            The value of the number
+	 * @param max
+	 *            The maximum that the specified {@code value} must be under
+	 * 
+	 * @return The specified {@code value}
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If {@code value} is {@code null} or is greater than {@code max}
+	 */
+	public static <T extends Number> T validateLessThanOrEqualTo(String name, T value, T max) {
+		if (value == null) {
+			doThrow("The argument %s must not be null.", name);
+		}
+
+		if (value.longValue() > max.longValue()) {
+			doThrow("The argument %s must not be greater than %d. Was: %d", name, value, max);
+		}
+
+		return value;
+	}
+
+	/**
+	 * Validate that a number is greater than a defined maximum.
+	 * 
+	 * @param name
+	 *            The name of the object being validated. Included in the exception message if one is thrown
+	 * @param value
+	 *            The value of the number
+	 * @param max
+	 *            The maximum that the specified {@code value} must be over
+	 * 
+	 * @return The specified {@code value}
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If {@code value} is {@code null} or is less than or equal to {@code max}
+	 */
+	public static <T extends Number> T validateGreaterThan(String name, T value, T max) {
+		if (value == null) {
+			doThrow("The argument %s must not be null.", name);
+		}
+
+		if (value.longValue() <= max.longValue()) {
+			doThrow("The argument %s must not be less than or equal to %d. Was: %d", name, value, max);
+		}
+
+		return value;
+	}
+
+	/**
+	 * Validate that a number is greater than or equal to a defined maximum.
+	 * 
+	 * @param name
+	 *            The name of the object being validated. Included in the exception message if one is thrown
+	 * @param value
+	 *            The value of the number
+	 * @param max
+	 *            The maximum that the specified {@code value} must be over
+	 * 
+	 * @return The specified {@code value}
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If {@code value} is {@code null} or is less than or equal to {@code max}
+	 */
+	public static <T extends Number> T validateGreaterThanOrEqualTo(String name, T value, T max) {
+		if (value == null) {
+			doThrow("The argument %s must not be null.", name);
+		}
+
+		if (value.longValue() < max.longValue()) {
+			doThrow("The argument %s must not be less than %d. Was: %d", name, value, max);
+		}
+
+		return value;
+	}
+
+	/**
+	 * Validate that a number falls within a specific range. The start and end of the range are inclusive.
+	 * 
+	 * @param name
+	 *            The name of the object being validated. Included in the exception message if one is thrown
+	 * @param value
+	 *            The value of the number
+	 * @param start
+	 *            The start of the range
+	 * @param end
+	 *            The end of the range
+	 * 
+	 * @return The specified {@code value}
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If {@code value} is {@code null} or falls outside of the range specified by {@code start} and {@code end}
+	 */
+	public static <T extends Number> T validateInRange(String name, T value, T start, T end) {
+		if (value == null) {
+			doThrow("The argument %s must not be null.", name);
+		}
+
+		long lValue = value.longValue();
+		if (lValue < start.longValue() || lValue > end.longValue()) {
+			doThrow("The argument %s must be in the range %d <= value <= %d. Was: %d", name, start, end, value);
+		}
+
+		return value;
+	}
+
+	private static void doThrow(String message, Object... parameters) {
+		throw new IllegalArgumentException(String.format(message, parameters));
 	}
 
 }
--- trunk/src/test/java/net/sf/xenqtt/XenqttUtilTest.java
+++ trunk/src/test/java/net/sf/xenqtt/XenqttUtilTest.java
@@ -2,6 +2,9 @@ package net.sf.xenqtt;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.Test;
 
 public class XenqttUtilTest {
@@ -87,6 +90,207 @@ public class XenqttUtilTest {
 	public void testQuickSplit_EmptyString() {
 		String[] values = XenqttUtil.quickSplit("", '/');
 		assertEquals(0, values.length);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateNotNull() {
+		Object o = new Object();
+		assertSame(o, XenqttUtil.validateNotNull("object", o));
+		XenqttUtil.validateNotNull("object", null);
+	}
+
+	@Test
+	public void testValidateNotEmpty() {
+		String str = "string";
+		assertSame(str, XenqttUtil.validateNotEmpty("string", str));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateNotEmpty_Blank() {
+		XenqttUtil.validateNotEmpty("string", "");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateNotEmpty_Null() {
+		String str = null;
+		XenqttUtil.validateNotEmpty("string", str);
+	}
+
+	@Test
+	public void testValidateNotEmpty_Array() {
+		Object[] os = new Object[1];
+		os[0] = new Object();
+		assertSame(os, XenqttUtil.validateNotEmpty("os", os));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateNotEmpty_ArrayOfSizeZero() {
+		Object[] os = new Object[0];
+		XenqttUtil.validateNotEmpty("os", os);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateNotEmpty_NullArray() {
+		Object[] os = null;
+		XenqttUtil.validateNotEmpty("os", os);
+	}
+
+	@Test
+	public void testValidateNotEmpty_Collection() {
+		Collection<Object> os = new ArrayList<Object>();
+		os.add(new Object());
+		assertSame(os, XenqttUtil.validateNotEmpty("os", os));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateNotEmpty_CollectionOfSizeZero() {
+		Collection<Object> os = new ArrayList<Object>();
+		XenqttUtil.validateNotEmpty("os", os);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateNotEmpty_NullCollection() {
+		Collection<Object> os = null;
+		XenqttUtil.validateNotEmpty("os", os);
+	}
+
+	@Test
+	public void testValidateLessThan() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(10);
+		assertSame(value, XenqttUtil.validateLessThan("integer", value, max));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateLessThan_Greater() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(5);
+		XenqttUtil.validateLessThan("integer", value, max);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateLessThan_Equal() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(7);
+		XenqttUtil.validateLessThan("integer", value, max);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateLessThan_Null() {
+		Integer max = Integer.valueOf(7);
+		XenqttUtil.validateLessThan("integer", null, max);
+	}
+
+	@Test
+	public void testValidateLessThanOrEqualTo() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(10);
+		assertSame(value, XenqttUtil.validateLessThanOrEqualTo("integer", value, max));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateLessThanOrEqualTo_Greater() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(5);
+		XenqttUtil.validateLessThanOrEqualTo("integer", value, max);
+	}
+
+	@Test
+	public void testValidateLessThanOrEqualTo_Equal() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(7);
+		assertSame(value, XenqttUtil.validateLessThanOrEqualTo("integer", value, max));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateLessThanOrEqualTo_Null() {
+		Integer max = Integer.valueOf(7);
+		XenqttUtil.validateLessThanOrEqualTo("integer", null, max);
+	}
+
+	@Test
+	public void testValidateGreaterThan() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(5);
+		assertSame(value, XenqttUtil.validateGreaterThan("integer", value, max));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateGreaterThan_Less() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(10);
+		XenqttUtil.validateGreaterThan("integer", value, max);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateGreaterThan_Equal() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(7);
+		XenqttUtil.validateGreaterThan("integer", value, max);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateGreaterThan_Null() {
+		Integer max = Integer.valueOf(7);
+		XenqttUtil.validateGreaterThan("integer", null, max);
+	}
+
+	@Test
+	public void testValidateGreaterThanOrEqualTo() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(5);
+		assertSame(value, XenqttUtil.validateGreaterThanOrEqualTo("integer", value, max));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateGreaterThanOrEqualTo_Less() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(10);
+		XenqttUtil.validateGreaterThanOrEqualTo("integer", value, max);
+	}
+
+	@Test
+	public void testValidateGreaterThanOrEqualTo_Equal() {
+		Integer value = Integer.valueOf(7);
+		Integer max = Integer.valueOf(7);
+		assertSame(value, XenqttUtil.validateGreaterThanOrEqualTo("integer", value, max));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateGreaterThanOrEqualTo_Null() {
+		Integer max = Integer.valueOf(7);
+		XenqttUtil.validateGreaterThanOrEqualTo("integer", null, max);
+	}
+
+	@Test
+	public void testValidateInRange() {
+		Integer start = Integer.valueOf(1);
+		Integer end = Integer.valueOf(10);
+		for (int i = 1; i <= 10; i++) {
+			Integer value = Integer.valueOf(i);
+			assertSame(value, XenqttUtil.validateInRange("range", value, start, end));
+		}
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateInRange_OutOfRange_Below() {
+		Integer start = Integer.valueOf(1);
+		Integer end = Integer.valueOf(10);
+		XenqttUtil.validateInRange("range", Integer.valueOf(0), start, end);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateInRange_OutOfRange_Above() {
+		Integer start = Integer.valueOf(1);
+		Integer end = Integer.valueOf(10);
+		XenqttUtil.validateInRange("range", Integer.valueOf(11), start, end);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testValidateInRange_OutOfRange_Null() {
+		Integer start = Integer.valueOf(1);
+		Integer end = Integer.valueOf(10);
+		XenqttUtil.validateInRange("range", null, start, end);
 	}
 
 }
