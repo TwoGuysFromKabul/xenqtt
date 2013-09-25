88cd0fd85a75850579d7986520c35df523bca4f7
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-09-25 21:04:08 +0000

    updated integration tests

--- src/test/java/net/sf/xenqtt/integration/AsyncMqttClientIT.java
+++ src/test/java/net/sf/xenqtt/integration/AsyncMqttClientIT.java
@@ -33,6 +33,7 @@ import net.sf.xenqtt.client.ReconnectionStrategy;
 import net.sf.xenqtt.client.Subscription;
 import net.sf.xenqtt.message.ConnectReturnCode;
 import net.sf.xenqtt.message.QoS;
+import net.sf.xenqtt.mockbroker.MockBroker;
 
 import org.junit.After;
 import org.junit.Before;
@@ -45,6 +46,14 @@ import org.mockito.MockitoAnnotations;
 
 public class AsyncMqttClientIT {
 
+	// FIXME [jim] - trailing slashes in topics should just be removed
+	// FIXME [jim] - make sure a/# subscripts to just "a" as well as all subtopics
+	// FIXME [jim] - duplicate // separators are just treated as one
+	// FIXME [jim] - an empty string is a valid topic
+	// FIXME [jim] - the client should send a pingreq if it hasn't heard from the server in ping timeout. this could happen if the client is just publishing qos
+	// 0 messages.
+	// FIXME [jim] - what is the correct behavior when you subscribe to a topic multiple times??
+
 	String badCredentialsUri = "tcp://q.m2m.io:1883";
 	String validBrokerUri = "tcp://test.mosquitto.org:1883";
 
@@ -53,6 +62,7 @@ public class AsyncMqttClientIT {
 	@Captor ArgumentCaptor<Subscription[]> subscriptionCaptor;;
 	@Captor ArgumentCaptor<PublishMessage> messageCaptor;;
 
+	MockBroker mockBroker;
 	AsyncMqttClient client;
 	AsyncMqttClient client2;
 
@@ -70,11 +80,9 @@ public class AsyncMqttClientIT {
 		if (client2 != null) {
 			client2.shutdown();
 		}
-	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+		if (mockBroker != null) {
+			mockBroker.shutdown(5000);
+		}
 	}
 
 	@Test
@@ -151,7 +159,21 @@ public class AsyncMqttClientIT {
 	@Test
 	public void testConnect_Credentials_Accepted() throws Exception {
 
-		fail("not implemented");
+		if (mockBroker == null) {
+			mockBroker = new MockBroker(null, 15, 0, true);
+			mockBroker.init();
+		}
+
+		mockBroker.addCredentials("user1", "password1");
+		validBrokerUri = "tcp://localhost:" + mockBroker.getPort();
+
+		client = new AsyncMqttClient(validBrokerUri, listener, reconnectionStrategy, 5, 0, 5);
+		client.connect("testclient2", true, 90, "user1", "password1");
+
+		verify(listener, timeout(5000)).connected(client, ConnectReturnCode.ACCEPTED);
+
+		client.disconnect();
+		verify(listener, timeout(5000)).disconnected(eq(client), isNull(Throwable.class), eq(false));
 	}
 
 	@Test
@@ -295,7 +317,43 @@ public class AsyncMqttClientIT {
 	@Test
 	public void testConnect_CredentialsAndWill_Accepted() throws Exception {
 
-		fail("not implemented");
+		if (mockBroker == null) {
+			mockBroker = new MockBroker(null, 15, 0, true);
+			mockBroker.init();
+		}
+
+		mockBroker.addCredentials("user1", "password1");
+		validBrokerUri = "tcp://localhost:" + mockBroker.getPort();
+
+		// connect and subscribe a client to get the will message
+		AsyncClientListener listener2 = mock(AsyncClientListener.class);
+		client2 = new AsyncMqttClient(validBrokerUri, listener2, reconnectionStrategy, 5, 0, 5);
+		client2.connect("testclient3", true, 90);
+		verify(listener2, timeout(5000)).connected(client2, ConnectReturnCode.ACCEPTED);
+		client2.subscribe(new Subscription[] { new Subscription("my/will/topic1", QoS.AT_LEAST_ONCE) });
+		verify(listener2, timeout(5000)).subscribed(same(client2), any(Subscription[].class), any(Subscription[].class), eq(true));
+
+		// connect and close a client to generate the will message
+		client = new AsyncMqttClient(validBrokerUri, listener, reconnectionStrategy, 5, 0, 5);
+		client.connect("testclient4", true, 90, "user1", "password1", "my/will/topic1", "it died dude", QoS.AT_LEAST_ONCE, false);
+		verify(listener, timeout(5000)).connected(client, ConnectReturnCode.ACCEPTED);
+		client.close();
+		verify(listener, timeout(5000)).disconnected(eq(client), isNull(Throwable.class), eq(false));
+
+		// verify the will message
+		verify(listener2, timeout(5000)).publishReceived(same(client2), messageCaptor.capture());
+		PublishMessage message = messageCaptor.getValue();
+		message.ack();
+		assertEquals("my/will/topic1", message.getTopic());
+		assertEquals("it died dude", message.getPayloadString());
+		assertEquals(QoS.AT_LEAST_ONCE, message.getQoS());
+		assertFalse(message.isDuplicate());
+		assertFalse(message.isRetain());
+
+		client2.disconnect();
+		verify(listener2, timeout(5000)).disconnected(same(client2), any(Throwable.class), eq(false));
+
+		verifyNoMoreInteractions(listener, listener2);
 	}
 
 	@Test
@@ -521,23 +579,37 @@ public class AsyncMqttClientIT {
 	@Test
 	public void testClose() throws Exception {
 
+		client = new AsyncMqttClient(validBrokerUri, listener, reconnectionStrategy, 5, 0, 5);
+		client.connect("testclient19", true, 90);
+		verify(listener, timeout(5000)).connected(client, ConnectReturnCode.ACCEPTED);
+		client.close();
+		verify(listener, timeout(5000)).disconnected(eq(client), isNull(Throwable.class), eq(false));
+	}
+
+	@Test
+	public void testDisconnect() throws Exception {
+
+		client = new AsyncMqttClient(validBrokerUri, listener, reconnectionStrategy, 5, 0, 5);
+		client.connect("testclient20", true, 90);
+		verify(listener, timeout(5000)).connected(client, ConnectReturnCode.ACCEPTED);
+		client.disconnect();
+		verify(listener, timeout(5000)).disconnected(eq(client), isNull(Throwable.class), eq(false));
+	}
+
+	@Test
+	public void testConnectionLost_FirstReconnectSucceeds() throws Exception {
+
 		fail("not implemented");
 	}
 
 	@Test
-	public void testConnectionLost_ReconnectSucceeds() throws Exception {
+	public void testConnectionLost_NotFirstReconnectSucceeds() throws Exception {
 
 		fail("not implemented");
 	}
 
 	@Test
 	public void testConnectionLost_AllReconnectsFail() throws Exception {
-
-		fail("not implemented");
-	}
-
-	@Test
-	public void testDisconnect() throws Exception {
 
 		fail("not implemented");
 	}
--- src/test/java/net/sf/xenqtt/integration/MockBrokerIT.java
+++ src/test/java/net/sf/xenqtt/integration/MockBrokerIT.java
@@ -15,38 +15,93 @@
  */
 package net.sf.xenqtt.integration;
 
+import static org.junit.Assert.*;
+import static org.mockito.AdditionalMatchers.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+import net.sf.xenqtt.client.AsyncClientListener;
+import net.sf.xenqtt.client.AsyncMqttClient;
+import net.sf.xenqtt.client.PublishMessage;
+import net.sf.xenqtt.client.Subscription;
+import net.sf.xenqtt.message.ConnectReturnCode;
+import net.sf.xenqtt.message.QoS;
 import net.sf.xenqtt.mockbroker.MockBroker;
 
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.MockitoAnnotations;
 
 public class MockBrokerIT extends AsyncMqttClientIT {
 
-	MockBroker broker;
-	String brokerUri;
-
+	// FIXME [jim] - need to add mock broker specific tests, if any
 	@Override
 	@Before
 	public void before() {
 
 		MockitoAnnotations.initMocks(this);
 
-		broker = new MockBroker(null, 15, 0, true);
-		broker.init();
-		brokerUri = "tcp://localhost:" + broker.getPort();
-		validBrokerUri = brokerUri;
-		badCredentialsUri = brokerUri;
-		broker.shutdown(1000);
+		mockBroker = new MockBroker(null, 15, 0, true);
+		mockBroker.init();
+		validBrokerUri = "tcp://localhost:" + mockBroker.getPort();
+		badCredentialsUri = validBrokerUri;
 
 		super.before();
 	}
 
-	@Override
-	@After
-	public void after() {
+	@Test
+	public void testSubscribeUnsubscribe_Wildcards() throws Exception {
 
-		super.after();
-		broker.shutdown(5000);
+		// connect publishing client
+		AsyncClientListener listener2 = mock(AsyncClientListener.class);
+		client2 = new AsyncMqttClient(validBrokerUri, listener2, reconnectionStrategy, 5, 0, 5);
+		client2.connect("testclient21", true, 90);
+		verify(listener2, timeout(5000)).connected(client2, ConnectReturnCode.ACCEPTED);
+
+		// connect client
+		client = new AsyncMqttClient(validBrokerUri, listener, reconnectionStrategy, 5, 0, 5);
+		client.connect("testclient22", true, 90);
+		verify(listener, timeout(5000)).connected(client, ConnectReturnCode.ACCEPTED);
+
+		// test subscribing standard
+		Subscription[] requestedSubscriptions = new Subscription[] { new Subscription("my/topic1", QoS.AT_LEAST_ONCE) };
+		client.subscribe(requestedSubscriptions);
+		verify(listener, timeout(5000)).subscribed(same(client), same(requestedSubscriptions), aryEq(requestedSubscriptions), eq(true));
+
+		client2.publish(new PublishMessage("my/topic1", QoS.AT_LEAST_ONCE, "abc"));
+		verify(listener, timeout(5000)).publishReceived(same(client), messageCaptor.capture());
+		assertEquals(QoS.AT_LEAST_ONCE, messageCaptor.getValue().getQoS());
+		messageCaptor.getValue().ack();
+
+		// test subscription to same topic via wildcard
+		messageCaptor = ArgumentCaptor.forClass(PublishMessage.class);
+		requestedSubscriptions = new Subscription[] { new Subscription("my/+", QoS.AT_MOST_ONCE) };
+		client.subscribe(requestedSubscriptions);
+		verify(listener, timeout(5000)).subscribed(same(client), same(requestedSubscriptions), aryEq(requestedSubscriptions), eq(true));
+
+		client2.publish(new PublishMessage("my/topic1", QoS.AT_LEAST_ONCE, "def"));
+		verify(listener, timeout(5000).times(2)).publishReceived(same(client), messageCaptor.capture());
+		assertEquals(QoS.AT_MOST_ONCE, messageCaptor.getValue().getQoS());
+		messageCaptor.getValue().ack();
+
+		// test unsubscribing wildcard
+		client.unsubscribe(new String[] { "my/+" });
+		verify(listener, timeout(5000)).unsubscribed(same(client), aryEq(new String[] { "my/+" }));
+
+		messageCaptor = ArgumentCaptor.forClass(PublishMessage.class);
+		client2.publish(new PublishMessage("my/topic1", QoS.AT_LEAST_ONCE, "ghi"));
+		verify(listener, timeout(5000).times(3)).publishReceived(same(client), messageCaptor.capture());
+		assertEquals(QoS.AT_LEAST_ONCE, messageCaptor.getValue().getQoS());
+		messageCaptor.getValue().ack();
+
+		// test unsubscribing standard
+		client.unsubscribe(new String[] { "my/topic1" });
+		verify(listener, timeout(5000)).unsubscribed(same(client), aryEq(new String[] { "my/topic1" }));
+
+		// disconnect
+		client.disconnect();
+		verify(listener, timeout(5000)).disconnected(eq(client), isNull(Throwable.class), eq(false));
+
+		verifyNoMoreInteractions(listener);
 	}
 }
