57b842510bac3a27951443eb5cb3b4f7f634b51b
impentaj <impentaj@4911e398-3a94-4a99-ae0e-6203f4ac0d84>
2013-10-01 14:58:47 +0000

    updated unit tests

--- src/test/java/net/sf/xenqtt/client/MqttClientConfigTest.java
+++ src/test/java/net/sf/xenqtt/client/MqttClientConfigTest.java
@@ -37,4 +37,56 @@ public class MqttClientConfigTest {
 	public void testSetConnectTimeout_LessThanZero() throws Exception {
 		config.setConnectTimeoutSeconds(-1);
 	}
+
+	@Test
+	public void testClone() throws Exception {
+
+		assertEquals(0, config.getBlockingTimeoutSeconds());
+		assertEquals(30000, config.getConnectTimeoutMillis());
+		assertEquals(30, config.getConnectTimeoutSeconds());
+		assertEquals(300, config.getKeepAliveSeconds());
+		assertEquals(30, config.getMessageResendIntervalSeconds());
+		assertEquals(50, ((ProgressiveReconnectionStrategy) config.getReconnectionStrategy()).getBaseReconnectMillis());
+		assertEquals(0, ((ProgressiveReconnectionStrategy) config.getReconnectionStrategy()).getCurrentRetry());
+		assertEquals(Integer.MAX_VALUE, ((ProgressiveReconnectionStrategy) config.getReconnectionStrategy()).getMaxNumberOfReconnects());
+		assertEquals(30000, ((ProgressiveReconnectionStrategy) config.getReconnectionStrategy()).getMaxReconnectMillis());
+		assertEquals(5, ((ProgressiveReconnectionStrategy) config.getReconnectionStrategy()).getProgressiveFactor());
+
+		MqttClientConfig configClone = config.clone();
+
+		assertEquals(0, configClone.getBlockingTimeoutSeconds());
+		assertEquals(30000, configClone.getConnectTimeoutMillis());
+		assertEquals(30, configClone.getConnectTimeoutSeconds());
+		assertEquals(300, configClone.getKeepAliveSeconds());
+		assertEquals(30, configClone.getMessageResendIntervalSeconds());
+		assertEquals(50, ((ProgressiveReconnectionStrategy) configClone.getReconnectionStrategy()).getBaseReconnectMillis());
+		assertEquals(0, ((ProgressiveReconnectionStrategy) configClone.getReconnectionStrategy()).getCurrentRetry());
+		assertEquals(Integer.MAX_VALUE, ((ProgressiveReconnectionStrategy) configClone.getReconnectionStrategy()).getMaxNumberOfReconnects());
+		assertEquals(30000, ((ProgressiveReconnectionStrategy) configClone.getReconnectionStrategy()).getMaxReconnectMillis());
+		assertEquals(5, ((ProgressiveReconnectionStrategy) configClone.getReconnectionStrategy()).getProgressiveFactor());
+
+		config.setBlockingTimeoutSeconds(1);
+		config.setConnectTimeoutSeconds(2);
+		config.setKeepAliveSeconds(3);
+		config.setMessageResendIntervalSeconds(4);
+		config.getReconnectionStrategy().connectionLost(null, null);
+
+		assertEquals(1, config.getBlockingTimeoutSeconds());
+		assertEquals(2000, config.getConnectTimeoutMillis());
+		assertEquals(2, config.getConnectTimeoutSeconds());
+		assertEquals(3, config.getKeepAliveSeconds());
+		assertEquals(4, config.getMessageResendIntervalSeconds());
+		assertEquals(1, ((ProgressiveReconnectionStrategy) config.getReconnectionStrategy()).getCurrentRetry());
+
+		assertEquals(0, configClone.getBlockingTimeoutSeconds());
+		assertEquals(30000, configClone.getConnectTimeoutMillis());
+		assertEquals(30, configClone.getConnectTimeoutSeconds());
+		assertEquals(300, configClone.getKeepAliveSeconds());
+		assertEquals(30, configClone.getMessageResendIntervalSeconds());
+		assertEquals(50, ((ProgressiveReconnectionStrategy) configClone.getReconnectionStrategy()).getBaseReconnectMillis());
+		assertEquals(0, ((ProgressiveReconnectionStrategy) configClone.getReconnectionStrategy()).getCurrentRetry());
+		assertEquals(Integer.MAX_VALUE, ((ProgressiveReconnectionStrategy) configClone.getReconnectionStrategy()).getMaxNumberOfReconnects());
+		assertEquals(30000, ((ProgressiveReconnectionStrategy) configClone.getReconnectionStrategy()).getMaxReconnectMillis());
+		assertEquals(5, ((ProgressiveReconnectionStrategy) configClone.getReconnectionStrategy()).getProgressiveFactor());
+	}
 }
